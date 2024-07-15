45776aa39773d95a1fa5628545e0e82ce8ef8098
rjpotter <rjpotter@uvm.edu>
2024-07-15 12:43:41 -0400

    Initial Commit

---
+++ Fetch Coding Challenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fetch Coding Challenge
+//
+//  Created by Ryan Potter on 7/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fetch Coding Challenge/Fetch_Coding_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Coding_ChallengeApp.swift
+//  Fetch Coding Challenge
+//
+//  Created by Ryan Potter on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_Coding_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
